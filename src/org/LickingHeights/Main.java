b5255955b16fbe6624f09ecf2498c2bf467ebdea
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-26 09:51:39 -0400

    added todo steps

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,7 +3,21 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
+        //todo declare variables a,b,c,y,discriminant,axisOfSymmetry,ParabolaFacing,x,y
+
+        //todo initialize manually
+
+        //todo da calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results
 
 
-    }
+
+   }
 }
