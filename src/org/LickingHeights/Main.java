1b81cd678018643dfc135cdf93d6f26516e04d30
is-an-cornotis-n-cor <isancor.yadessa.2023@student.lhschools.org>
2019-09-26 10:05:55 -0400

    did calculations for axis of symmetry and discriminant

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -11,9 +11,10 @@ public class Main {
         b=3;
         c=1;
         //todo da calculations
-
+        axisOfSymmetry= -b/(2*a);
+        discriminant= (b*b)-4*(a*c);
         //todo print results
-
+      
         //todo not in code check results
 
         //todo initialize scanner and redo the initialize area
